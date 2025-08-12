7189f6761c90269a85192e42f063f77b126d327b
Lucas Luz <lucasdias200lucasdias@gmail.com>
2025-08-12 20:59:21 -0300

    fix: add rest annotation controller

--- src/main/java/dev/project/api/controller/UserController.java
+++ src/main/java/dev/project/api/controller/UserController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import dev.project.api.dto.UserDTO;
 import dev.project.api.service.UserService;
 import jakarta.validation.Valid;
 
+@RestController
 public class UserController {
 
     @Autowired
